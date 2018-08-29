fc1522af39c9e836a6a92d2815be24a9d05c1500
guixiangyu <guixiangyu1@gmail.com>
2018-08-29 15:59:39 +0800

    test

--- model/config.py
+++ model/config.py
@@ -78,9 +78,9 @@ class Config():
     use_pretrained = True
 
     # dataset
-    filename_dev = "data/valid1.txt"
+    filename_dev = "data/test1.txt"
     filename_test = "data/test1.txt"
-    filename_train = "data/train1.txt"
+    filename_train = "data/valid1.txt"
 
     # filename_dev = filename_test = filename_train = "data/test.txt" # test
 
